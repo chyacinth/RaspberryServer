2423288050def8d835721dd5b7aebf597b60a27e
chyacinth <hyacinth@hyacinthdeMacBook-Pro.local>
2017-05-25 23:57:42 +0800

    厉害

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 var routes = require('./routes/index');
+var session = require('express-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +24,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set the session
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
 routes(app);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,26 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('raspdb.db');
 var wifi = require('node-wifi');
 var io = require('socket.io-client');
-var socket = io('http://http://45.63.50.188/?sessionID=3RyZ4y3XjcsgHKxKCzymgCYMizMReSrO');
+var cookies = '';
+var request = require('superagent');
+var setCookie = require('set-cookie-parser');
+var real_sid;
+var socket;
+
 
 wifi.init({
     iface : 'en0' // network interface, choose a random wifi interface if set to null
 });
 
-function lookUpAndSend() {
-    db.serialize(function() {
-        db.each("SELECT * FROM humidity WHERE id = (SELECT MAX(id)  FROM humidity);", function (err, row) {
-            //send data
-            console.log(row.id + ": " + row.data);
-
-
+function lookUpAndSend(req, res) {
+    if (socket) {
+        db.serialize(function () {
+            db.each("SELECT * FROM humidity WHERE id = (SELECT MAX(id)  FROM humidity);", function (err, row) {
+                //send data
+                console.log(row.id + ": " + row.data);
+            });
         });
-    });
-    
+    }
 }
 
 function sample (req, res) {
@@ -41,7 +45,49 @@ function sample (req, res) {
 
 function userLogin(req, res)
 {
-    
+    request
+        .post('http://45.63.50.188/login/login')
+        .send({ username: req.body.username, password: req.body.password }) // sends a JSON post body
+        .set('Accept', 'application/json')
+        .end(function(err, result){
+            // Calling the end function will send the request
+            if (err)
+            {
+                res.redirect('/');
+            }
+            if (result)
+            {
+                if (result.body.status === 0)
+                {
+                    res.redirect('/');
+                }
+                else
+                {
+                    cookies = setCookie.parse(result, {
+                        decodeValues: true  // default: true
+                    });
+                    var signature = require( "cookie-signature" );
+
+                    prefix = "s:";
+
+                    real_sid = cookies[0].value.replace( prefix, "" );
+                    real_sid = signature.unsign( real_sid, 'keyboard cat');
+
+                    socket = io('http://45.63.50.188/?sessionID=' + real_sid);
+                    socket.on('nosession', function() {
+                        console.log('nosession!');
+                        socket.close();
+                    });
+                    socket.on('hassession', function() {
+                        console.log('hasSession!');
+                        socket.close();
+                    });
+
+                    console.log(cookies);
+                    console.log("成功登陆！");
+                }
+            }
+        });
 }
 
 module.exports = function(app) {
